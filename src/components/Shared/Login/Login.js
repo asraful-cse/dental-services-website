a5506d6f345cb8ad56377153eb0f0f74731abc6b
asraful196 <asraful.01110@gmail.com>
2021-10-19 12:45:35 +0600

    create email added

--- src/components/Shared/Login/Login.js
+++ src/components/Shared/Login/Login.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import useAuth from '../../../hooks/useAuth';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, updateProfile} from "firebase/auth";
 import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
+
+
 
 
 const Login = () => {
     const { googleSignIn } = useAuth();
     const auth = getAuth();
+
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     const [isLogin, setIsLogin] = useState(false);
+
 
     const toggleLogin = e => {
         setIsLogin(e.target.checked)
@@ -79,7 +83,6 @@ const Login = () => {
         updateProfile(auth.currentUser, { displayName: name })
             .then(result => { })
     }
-
     const verifyEmail = () => {
         sendEmailVerification(auth.currentUser)
             .then(result => {
@@ -88,58 +91,52 @@ const Login = () => {
     }
     return (
         <div className="main d-flex justify-content-center align-items-center">
-
-            <div className="mx-5 container" style={{ height: '600px', width: '480px' }} >
-
-                <form onSubmit={handleRegistration} className="" >
+            <div className="mx-5 container" style={{ height: '600px', width: '480px' }}>
+                <form onSubmit={handleRegistration}>
                     <h4 className="text-dark text-center fw-bold"><i class="fas fa-user-edit text-warning"></i> <br />
                         Hi !!  User  Please {isLogin ? <strong className="text-success">Login</strong> : <strong className="text-danger"> Register </strong>}
                     </h4>
                     {!isLogin && <div className="row mb-3">
-                        <label htmlFor="inputName" className="col-sm-2 col-form-label mx-1">Name</label>
+                        <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
                         <div className="col-sm-10">
                             <input type="text" onBlur={handleNameChange} className="form-control" id="inputName" placeholder="Your Name" />
                         </div>
                     </div>}
                     <div className="row mb-3">
-                        <label htmlFor="inputEmail3" className="col-sm-2 col-form-label mx-1">Email</label><br />
+                        <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
                         <div className="col-sm-10">
-                            <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" placeholder="Your email address" required />
+                            <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" required />
                         </div>
                     </div>
                     <div className="row mb-3">
-                        <label htmlFor="inputPassword3" className="col-sm-2 col-form-label mx-1">Password</label>
+                        <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
                         <div className="col-sm-10">
-                            <input type="password" onBlur={handlePasswordChange} className="form-control" id="inputPassword3" placeholder="Your password" required />
+                            <input type="password" onBlur={handlePasswordChange} className="form-control" id="inputPassword3" required />
                         </div>
                     </div>
                     <div className="row mb-3">
                         <div className="col-sm-10 offset-sm-2">
-                        <div className="row mb-3 text-danger text-center">{error}</div>
                             <div className="form-check">
                                 <input onChange={toggleLogin} className="form-check-input" type="checkbox" id="gridCheck1" />
                                 <label className="form-check-label" htmlFor="gridCheck1">
-                                    Already Registered Login now ?
+                                    Already Registered?
                                 </label>
                             </div>
                         </div>
+                        <div className="row mb-3 text-danger d-flex justify-content-center align-items-center">{error}</div>
                     </div>
                     
-                   
-                </form>
-                <div className="from-group mt-1 d-flex justify-content-center align-items-center  ">
-                <button type="submit" className="btn btn-primary d-flex justify-content-center align-items-center">     
-                        {isLogin ? 'Login' : 'Register'}   
+                    <button type="submit" className="btn btn-primary">
+                        {isLogin ? 'Login' : 'Register'}
                     </button>
-                </div>
-              
-                <div>----------------------------- OR -----------------------</div>
-
-                <div className="from-group mt-1 d-flex justify-content-center align-items-center  ">
+                </form>
+                <div>--------------------------- OR -----------------------</div>
+                <div className="from-group d-flex justify-content-center align-items-center ">
                     <button onClick={googleSignIn} className="btn btn-primary " ><i class="fab fa-google fw-5 text-warning">  </i>  sign in</button>
                 </div>
             </div>
         </div>
+
     );
 };
 
