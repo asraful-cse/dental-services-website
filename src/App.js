3b5cf087b3dc0c6504add2358f21da56b5c97def
asraful196 <asraful.01110@gmail.com>
2021-11-26 14:50:07 +0600

    apply page updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ApplyPage from "./components/ApplyPage/ApplyPage";
 import AboutUs from "./components/Home/AboutUs/AboutUs";
 import Contacts from "./components/Home/Contacts/Contacts";
@@ -17,45 +13,44 @@ import Navbar from "./components/Shared/Navbar/Navbar";
 import NotFound from "./components/Shared/NotFound/NotFound";
 import AuthProvider from "./Context/AuthProvider";
 
-
 function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <Navbar></Navbar>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-          <Route path="/services">
-            <Service></Service>
-          </Route>
-          <Route path="/about">
-            <AboutUs />
-          </Route>
-          <Route path="/contacts">
-            <Contacts />
-          </Route>
-          <PrivetRoute path="/service_detail/:id">
-            <ServiceDetail></ServiceDetail>
-          </PrivetRoute>
-          <Route path="/apply">
-            <ApplyPage></ApplyPage>
-          </Route>
-          <Route path="*">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-        <Footer></Footer>
-      </Router>
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			<Router>
+				<Navbar></Navbar>
+				<Switch>
+					<Route exact path="/">
+						<Home></Home>
+					</Route>
+					<Route path="/home">
+						<Home></Home>
+					</Route>
+					<Route path="/login">
+						<Login></Login>
+					</Route>
+					<Route path="/services">
+						<Service></Service>
+					</Route>
+					<Route path="/about">
+						<AboutUs />
+					</Route>
+					<Route path="/contacts">
+						<Contacts />
+					</Route>
+					<PrivetRoute path="/service_detail/:id">
+						<ServiceDetail></ServiceDetail>
+					</PrivetRoute>
+					<Route path="/apply">
+						<ApplyPage></ApplyPage>
+					</Route>
+					<Route path="*">
+						<NotFound></NotFound>
+					</Route>
+				</Switch>
+				<Footer></Footer>
+			</Router>
+		</AuthProvider>
+	);
 }
 
 export default App;
